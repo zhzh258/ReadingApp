00f42c587321b09fe5cda794f699f48361aa138d
Zhaozhan <nonox530042@gmail.com>
2024-03-25 03:12:25 -0400

    built frontend login

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import express, { Express } from "express";
-import authRouter from "./api/authApi";
+import cors from "cors"
+import authRouter from "./api/auth";
 import { connectDB } from "./database/database"; // Assuming default export is not used
 
 const port: number = 4000;
 const app: Express = express();
 
+app.use(cors())
 app.use(express.json()); // For parsing application/json
 app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
 app.use(authRouter);
